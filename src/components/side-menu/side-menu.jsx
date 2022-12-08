f4ee18edc1432b23d514e51fb0343f45048f5b87
Doshik <sanzhar_022@mail.ru>
2022-12-08 22:49:29 +0600

    sidebar icons

--- src/components/side-menu/side-menu.jsx
+++ src/components/side-menu/side-menu.jsx
@@ -1,40 +1,68 @@
 import './side-menu.css';
 import { Menu, Layout } from 'antd'; // or 'antd/dist/antd.less'
 import { useNavigate } from 'react-router-dom';
+import {
+  HomeOutlined,
+  DesktopOutlined,
+  TeamOutlined,
+  UserAddOutlined,
+  UserOutlined,
+  MenuOutlined,
+  FormOutlined,
+  DatabaseOutlined,
+  ContainerOutlined,
+  BlockOutlined,
+  LaptopOutlined,
+} from '@ant-design/icons';
 const { Sider } = Layout;
 
 const SideMenu = () => {
-	const navigate = useNavigate();
-	return (
-		<Sider width={280}>
-			<Menu
-				className="sidebarMenu"
-				mode="inline"
-				onClick={({ key }) => {
-					navigate(key);
-				}}
-				defaultSelectedKeys={[window.location.pathname]}
-				style={{
-					height: '100%',
-					borderRight: 0,
-					background: '#00a1dc',
-				}}
-				items={[
-					{ label: 'Главная', key: '/' },
-					{ label: 'Заявки на ремонт', key: '/repair_app' },
-					{ label: 'Оборудование в ремонте', key: '/repair_equip' },
-					{ label: 'Каталог названий оборудования' },
-					{ label: 'Компьютерная техника' },
-					{ label: 'Оборудование на рабочем месте' },
-					{ label: 'Элементы компьтерной техники' },
-					{ label: 'Каталог рабочих мест' },
-					{ label: 'Сотрудники' },
-					{ label: 'Должности' },
-					{ label: 'Регистрация сотрудника', key: '/register' },
-				]}
-			/>
-		</Sider>
-	);
+  const navigate = useNavigate();
+  return (
+    <Sider width={290} style={{ marginRight: 1 }}>
+      <Menu
+        className="sidebarMenu"
+        mode="inline"
+        onClick={({ key }) => {
+          navigate(key);
+        }}
+        defaultSelectedKeys={[window.location.pathname]}
+        style={{
+          height: '100%',
+          borderRight: 0,
+          background: '#00a1dc',
+        }}
+        items={[
+          { label: 'Главная', key: '/', icon: <HomeOutlined /> },
+          {
+            label: 'Заявки на ремонт',
+            key: '/repair_app',
+            icon: <FormOutlined />,
+          },
+          {
+            label: 'Оборудование в ремонте',
+            key: '/repair_equip',
+            icon: <DatabaseOutlined />,
+          },
+          {
+            label: 'Каталог названий оборудования',
+            icon: <ContainerOutlined />,
+          },
+          { label: 'Компьютерная техника', icon: <DesktopOutlined /> },
+          { label: 'Оборудование на рабочем месте', icon: <LaptopOutlined /> },
+          { label: 'Элементы компьтерной техники', icon: <BlockOutlined /> },
+          { label: 'Каталог рабочих мест', icon: <MenuOutlined /> },
+          { label: 'Сотрудники', icon: <TeamOutlined /> },
+          { label: 'Должности', icon: <UserOutlined /> },
+          {
+            label: 'Регистрация сотрудника',
+            key: '/register',
+            icon: <UserAddOutlined />,
+          },
+        ]}
+      />
+    </Sider>
+  );
 };
 
 export default SideMenu;
