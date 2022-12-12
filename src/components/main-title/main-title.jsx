da8c6c57af2a3ce55208b0738efe8938d5381dd9
Doshik <sanzhar_022@mail.ru>
2022-12-13 01:45:07 +0600

    main title

--- src/components/main-title/main-title.jsx
+++ src/components/main-title/main-title.jsx
@@ -1,11 +1,18 @@
 import './main-title.css';
+import titlePNG from '../../resources/img/titlePNG.png';
 
 const MainTitle = () => {
-	return (
-		<div className="main-title">
-			<h2>Добро пожаловать, USER</h2>
-		</div>
-	);
+  return (
+    <div className="main-title">
+      <div>
+        <h2>Добро пожаловать, USER</h2>
+      </div>
+
+      <div className="d-flex justify-content-center">
+        <img className="titlePNG " src={titlePNG} alt="titlePNG" />
+      </div>
+    </div>
+  );
 };
 
 export default MainTitle;
